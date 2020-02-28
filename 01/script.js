2cff13fe9ff6fe5a7dd1b0afa02df4d2b7fc05fb
Victor <magnum4520@gmail.com>
2020-02-28 19:01:22 +0200

    Это первый коммит

---
+++ 01/script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+let a = 5;
+console.log(a);
+
+
+// let persone = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(persone["name"]);
+
+// let arr = ["plum.png",'orange.jpg','apple.bmp'];
+
+// console.log(arr[0]);
+
+// alert('Hello World');
+
+// let answer = confirm('Are you here?');
+
+// console.log(answer);
+
+// let answer = +prompt('Есть ли вам 18?', "да");
+
+// console.log(typeof('answer'));
+ 
+// console.log('arr' + +' - object')
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2"=== 2);
+
+let isChecked = true,
+    isClose = false;
+
+    console.log(isChecked && isClose);
